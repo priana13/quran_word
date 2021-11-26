9e60be8872c15f26c40aac305307b72026c75d40
priana13 <herian.sap@gmail.com>
2021-11-27 06:17:53 +0700

    tambah admin lte dan hal master

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+{{-- 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -13,5 +13,8 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+
+    <h2 class="h2 text-red">Dashboard</h2>
+
 </x-app-layout>
