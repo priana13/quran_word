e811a9be7addd359bc9c91949d6f445054543379
priana13 <herian.sap@gmail.com>
2022-03-08 03:05:55 +0700

    delete breadcrumb

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -238,10 +238,10 @@
           @endif
           </div><!-- /.col -->
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
+            <!-- <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
+            </ol> -->
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
