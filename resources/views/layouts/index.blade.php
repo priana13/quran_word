7a6f44ff12d6756712d5607494cdab00702c1a3e
priana13 <herian.sap@gmail.com>
2025-02-26 12:07:49 +0700

    hapus avatar

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -33,10 +33,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/" class="nav-link">Donasi</a>
+        <a href="/" class="nav-link">Halaman Depan</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">About Us</a>
+        <a href="#" class="nav-link">History</a>
       </li>
     </ul>
 
@@ -131,7 +131,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ url('/') }}" class="brand-link">
       <img src="{{asset('dist/img')}}/AdminLTELogo.png" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">QuranKu</span>
     </a>
